ee283373b5d1c2953515625fa0a4fe08e05ceebd
joerg <joerg@localhost.(none)>
2009-10-21 21:30:36 +0200

    first views

--- lqnDemo/lqndemo/views.py
+++ lqnDemo/lqndemo/views.py
@@ -6,5 +6,22 @@ from repoze.bfg.chameleon_zpt import get_template
 
 def index(context,request):
     master = get_template('templates/master.pt')
-    return rtr('templates/test.pt',master=master)
+    return rtr('templates/index.pt',master=master)
+
+def send(context,request):
+    if request.POST.has_key('amount'):
+        print 'sending something'
+    master = get_template('templates/master.pt')
+    return rtr('templates/send.pt',master=master)
+
+def receive(context,request):
+    master = get_template('templates/master.pt')
+    return rtr('templates/receive.pt',master=master)
+
+def transactions(context,request):
+    master = get_template('templates/master.pt')
+    return rtr('templates/transactions.pt',master=master)
+
+
+
 
