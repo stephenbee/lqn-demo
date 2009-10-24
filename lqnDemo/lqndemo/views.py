40e8860cacb0cd31eca1779ac579be342ebab02f
joerg <joerg@localhost.(none)>
2009-10-24 11:48:07 +0200

    receiving works

--- lqnDemo/lqndemo/views.py
+++ lqnDemo/lqndemo/views.py
@@ -44,7 +44,34 @@ def send(context,request):
 def receive(context,request):
     master = get_template('templates/master.pt')
     logged_in = authenticated_userid(request)
-    return rtr('templates/receive.pt',context=context,request=request,master=master,logged_in=logged_in,message=None)
+    accounts = context['accounts']
+    errors = {}
+    message = ''
+    post = request.POST
+    if post.has_key('amount'):
+        amount = post.get('amount','')
+        try:
+            amount = int(amount)
+        except ValueError:
+            errors['amount'] = 'not a valid number'
+        if amount <= 0:
+            errors['amount'] = 'amount needs be at least 1'
+        source = post.get('source','')    
+        if not accounts.has_key(source):
+            errors['source'] = 'not a valid account'                
+        else:
+            pin = post.get('pin','')
+            if pin != accounts.get(source).password:
+                errors['pin'] = 'invalid password'
+        if errors:
+            message= 'please correct the errors'
+        else:       
+            tacc = accounts.get(logged_in)
+            sacc = accounts.get(source)
+            sacc.transfer(logged_in,amount)
+            return rtr('templates/paid.pt',context=context,request=request,master=master,logged_in=logged_in,source=sacc,target=tacc,amount=amount,message=message)
+
+    return rtr('templates/receive.pt',context=context,request=request,master=master,logged_in=logged_in,message=None,errors=errors)
 
 def transactions(context,request):
     master = get_template('templates/master.pt')
